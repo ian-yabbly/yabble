be9a906b0ba395b5883f7153dea3b4701972595d
Ian Shafer <ian@yabbly.com>
2014-01-14 11:36:41 -0800

    wip

---
+++ web/src/main/jvm/me/yabble/web/server/image-handler.scala
@@ -0,0 +1,114 @@
+package me.yabble.web.server
+
+import me.yabble.common.Predef._
+import me.yabble.common.Log
+import me.yabble.service._
+import me.yabble.service.model._
+import me.yabble.web.service._
+import me.yabble.web.template.VelocityTemplate
+
+import com.sun.net.httpserver._
+
+import org.apache.commons.io.IOUtils
+import org.apache.http.impl.cookie.DateUtils
+
+import org.springframework.util.AntPathMatcher
+
+import java.io.IOException
+import java.util.regex.Pattern
+
+import scala.collection.JavaConversions._
+
+class ImageHandler(
+    val sessionService: SessionService,
+    val userService: IUserService,
+    val encoding: String,
+    private val imageService: ImageService)
+  extends Handler
+{
+  def VERSION_PATTERN = Pattern.compile("^/s/v-[\\p{Alnum}_-]+(/.*)$")
+
+  private val pathPatterns = List("/i/{id}")
+
+  override def maybeHandle(exchange: HttpExchange): Boolean = {
+    val pathMatcher = new AntPathMatcher()
+    val path = noContextPath(exchange)
+
+    pathPatterns
+        .zipWithIndex
+        .find(t => pathMatcher.`match`(t._1, path))
+        .map(t => t._2 match {
+          case 0 => index(exchange, pathMatcher.extractUriTemplateVariables(t._1, path).toMap)
+          case _ => error(s"Unexpected match [${t._1}]")
+        })
+        .isDefined
+  }
+
+  def index(exchange: HttpExchange, pathVars: Map[String, String]) {
+    val id = pathVars("id")
+    optional2Option(imageService.optional(id)) match {
+      case Some(i) => {
+      }
+      case None => {
+        plainTextResponse(exchange, Some("Image not found [%s]".format(id)), 404)
+      }
+    }
+
+/*
+    val path = noContextPath(exchange)
+    val m = VERSION_PATTERN.matcher(path)
+
+    var isVersioned = false
+    val resourcePath = if (m.matches()) {
+          isVersioned = true
+          m.group(1)
+        } else {
+          path.substring("/s".length)
+        }
+
+    val contentType = if (path.toLowerCase().endsWith(".css")) {
+          Some("text/css; charset=%s".format(encoding))
+        } else if (path.toLowerCase().endsWith(".less")) {
+          Some("text/plain")
+        } else if (path.toLowerCase().endsWith(".js")) {
+          Some("application/javascript; charset=%s".format(encoding))
+        } else if (path.toLowerCase().endsWith(".gif")) {
+          Some("image/gif")
+        } else if (path.toLowerCase().endsWith(".jgp")) {
+          Some("image/jpeg")
+        } else {
+          None
+        }
+
+    try {
+      val startMs = System.currentTimeMillis()
+      contentType.foreach(t => exchange.getResponseHeaders.set("Content-Type", t))
+      if (isVersioned) {
+        exchange.getResponseHeaders.set("Cache-Control", "max-age=31536000, public")
+      }
+
+      val resource = resourceLoader.getResource(staticBaseResourcePath + resourcePath)
+
+      try {
+        exchange.getResponseHeaders.set("Last-Modified", DateUtils.formatDate(new java.util.Date(resource.lastModified())))
+      } catch {
+        case e: IOException => log.warn(e.getMessage, e)
+      }
+
+      exchange.sendResponseHeaders(200, resource.contentLength())
+      // TODO try/finally
+      IOUtils.copy(resource.getInputStream, exchange.getResponseBody)
+      resource.getInputStream.close()
+    } catch {
+      case e: Exception => {
+        log.error(e.getMessage, e)
+        try {
+          exchange.getResponseBody.close()
+        } catch {
+          case e: Exception => log.error(e.getMessage, e)
+        }
+      }
+    }
+*/
+  }
+}
